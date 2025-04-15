adacffe4bf77cacb5a30328a5ac12af5965f059e
NguyennHoangg <nguyenhuyhoang270804@gmail.com>
2025-04-15 21:08:54 +0700

    fix index root

--- javascript/userController.js
+++ javascript/userController.js
@@ -71,3 +71,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+document.getElementById('logout').addEventListener('click', () => {
+    // Xóa dữ liệu người dùng khỏi localStorage
+    localStorage.removeItem('loggedInUser');
+
+    // Xóa tên người dùng khỏi giao diện
+    document.getElementById('user-name').textContent = '';
+
+    alert('Bạn đã đăng xuất!');
+});
